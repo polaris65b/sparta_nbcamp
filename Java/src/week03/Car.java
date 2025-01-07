710bad9544fc2e1b230d5699a6b99dd5c3e4ed46
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-07 17:50:19 +0900

    3-3 객체 생성

--- Java/src/week03/Car.java
+++ Java/src/week03/Car.java
@@ -2,13 +2,21 @@ package week03;
 
 public class Car {
     // 필드 영역
+    // 1) 고유 데이터 영역
     String compangy;    // 자동차 회사
     String model;       // 자동차 모델
     String color;       // 자동차 색
     double price;       // 자동차 가격
+
+    // 2) 상태 데이터 영역
     double speed;       // 자동차 속도, km/h
     char gear;          // 기어 상태(P, R, N, D)
     boolean lights;     // 자동차 조명의 상태
+
+    // 3) 객체 데이터 영역
+    Tire tire;
+    Door door;
+    Handle handle;
 
     // 생성자 영역
     // 생성자 : 처음 객체가 생성될 때(instance화) 어떤 로직을 수행해야 하며, 어떤 값이 필수로 들어와야하는지 정의.
@@ -18,42 +26,36 @@ public class Car {
     }
 
     // 메서드 영역
-
     // gasPedal
-    // input : kmh
-    // output : speed
+    // input : kmh      output : speed
     double gasPedal(double kmh){
         speed = kmh;
         return speed;
     }
 
     // breakePedal
-    // input : x
-    // ouput : speed
+    // input : x        ouput : speed
     double breakPedal() {
         speed = 0;
         return speed;
     }
 
     // changGear
-    // input : gear(char type)
-    // output : gear(char type)
+    // input : gear(char type)      output : gear(char type)
     char changeGear(char type){
         gear = type;
         return gear;
     }
 
     // onOffLight
-    // input : x
-    // output : lights(boolean)
+    // input : x                    output : lights(boolean)
     boolean onOffLights (){
         lights = !lights;
         return lights;
     }
 
     // horn
-    // input : x
-    // output : x
+    // input : x                    output : x
     void horn(){
         System.out.println("빵빵!");
     }
