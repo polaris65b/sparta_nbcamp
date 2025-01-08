eb458fba171de2776b85a88216846c5174b9d6e4
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-08 21:01:28 +0900

    다형성

--- Java/src/week03/inter/Main.java
+++ Java/src/week03/inter/Main.java
@@ -1,43 +1,43 @@
 package week03.inter;
 
-public class Main extends D implements C {
-
-    @Override
-    public void a() {
-        System.out.println("A");
-    }
-
-    @Override
-    public void b() {
-        System.out.println("B");
-    }
-
-    @Override
-    void d() {
-        super.d();
-    }
-
+public class Main {
     public static void main(String[] args) {
-        Main main = new Main();
-        main.a();
-        main.b();
-        main.d();
-    }
-}
 
-interface A {
-    void a();
-}
+        // LG TV 구현체를 조작
+        // 자동 형 변환
+        MultiRemoteController mrc = new LgTv("LG");
+        mrc.turnOnOff();
+        mrc.volumeUp();
+        mrc.channelDown();
+        mrc.channelUp();
+        mrc.turnOnOff();
 
-interface B {
-    void b();
-}
+        // 조작 대상을 Samsung TV로 교체
+        System.out.println("\n<Samsung TV로 교체>");
+        // 자동 형 변환
+        mrc = new SamsungTv("Samsung");
+        mrc.turnOnOff();
+        mrc.channelUp();
+        mrc.volumeDown();
+        mrc.volumeUp();
+        mrc.turnOnOff();
 
-interface C extends A, B {
-}
+        // 매개변수, 반환타입 다형성 체크
+        System.out.println("\n<매개변수, 반환타입 다형성 체크>");
 
-class D {
-    void d() {
-        System.out.println("D");
+        MultiRemoteController samsung = mrc.getTV(new SamsungTv("Samsung"));
+        samsung.turnOnOff();
+
+        SamsungTv samsungTv = (SamsungTv) samsung;
+        samsungTv.turnOnOff();
+
+
+        System.out.println();
+        MultiRemoteController lg = mrc.getTV(new LgTv("LG"));
+        lg.turnOnOff();
+
+        LgTv lgTv = (LgTv) lg;
+        lgTv.turnOnOff();
+
     }
 }
