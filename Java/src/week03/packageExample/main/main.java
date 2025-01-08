1375e27be97f39cdf4b94b9a01c138ca2ae2b61d
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-08 14:59:50 +0900

    import

--- Java/src/week03/packageExample/main/main.java
+++ Java/src/week03/packageExample/main/main.java
@@ -1,13 +1,11 @@
 package week03.packageExample.main;
 
+import week03.packageExample.pk1.Car;
+
 public class main {
     public static void main(String[] args) {
-        // 클래스의 일부분이며, 하위 패키지를 도트(.)로 구분한다.
-
-        week03.packageExample.pk1.Car car1 = new week03.packageExample.pk1.Car();
-        week03.packageExample.pk2.Car car2 = new week03.packageExample.pk2.Car();
-
+        Car car1 = new Car();
         car1.horn();
-        car2.horn();
+//        week03.packageExample.pk2.Car
     }
 }
