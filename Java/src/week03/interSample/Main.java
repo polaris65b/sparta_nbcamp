4bbec9645c5ebe4bb6c431a358213d3d6b4d7587
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-08 20:40:07 +0900

    Update Main.java

--- Java/src/week03/interSample/Main.java
+++ Java/src/week03/interSample/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // A 인터페이스에 구현체 B 대입
         A a1 = new B();
         a1.a();
+        // a1은 인터페이스 A타입(자동 형변환)이기 때문에, a() 메서드만 가지고있다.
         // a1.b(); // 불가능
 
         System.out.println("\nB 강제 타입변환");
