24cd5f45ecdc75f31419e43f71b9f1d2cfe33b53
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-08 10:17:39 +0900

    Create Main.java

---
+++ Java/src/week03/sample/Main.java
@@ -0,0 +1,23 @@
+package week03.sample;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        // 메서드 호출 : main.getClass()
+        System.out.println("main.getClass() = " + main.getNumber());
+        System.out.println("main.getClass() = " + main.getNumber());
+        System.out.println("main.getClass() = " + main.getNumber());
+    }
+
+    public int getNumber() {
+        int number = 1; // 지역 변수
+        number += 1;
+        return number; // 메서드 종료되면 지역변수 제거됨
+    }
+}
+
+// 출력
+//main.getNumber() = 2
+//main.getNumber() = 2
+//main.getNumber() = 2
